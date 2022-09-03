42c1eb5c944d175c2b1d1f8189bedf1a1b0a9081
Jacqueline Bybee <89400113+jackie3982@users.noreply.github.com>
2022-09-02 19:31:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,8 +13,29 @@
 using namespace std;
 
 
+/**
+ * Takes user entered number and finds the sum of the 1 to n
+ *
+ * @param int n User entered number
+ * @pre User entered a number for n
+ * @return int Sum of 1 to n
+ * 
+ */
+int sum(int n);
+
 int main() {
   int n = 0;
-  cout << "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+  cout << "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" << "Enter a number pls.\n";
+  cin >> n;
+  cout << endl << sum(n) << endl;
+  
   return 0;
+}
+
+int sum(int n){
+  int total = 0;
+  for( int i = 0; i < n; i++){
+    total += i;
+  }
+  return total;
 }
